6ca93ef4044f311f1fd923dfd25da031ce9c3a9f
Oleg Smorodin <benmaker@mail.ru>
2023-09-17 02:52:29 +0300

    added info Sem2Task 13, 15

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -2,34 +2,59 @@
 //  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 
-// Console.WriteLine("Введите число: ");
-// int num = int.Parse(Console.ReadLine() ?? "0");
-// double digit = Math.Log10(num);
-// digit = (int)digit;
-// if (digit > 1){
-//     int count = 1;
-//     int res = num;
-//     while (count < digit -1){
-//         res = res/10;
-//         count++;
+// int Getnum()
+// {
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// Получаем число и номер разряда
+// Console.Write("Введите число: ");
+// int num = Getnum();
+// Console.Write("Введите номер разряда: ");
+// int digit = Getnum();
+// // Метод для получения числа искомого разряда
+// int GetDigit(int num, int numOfDigit)
+// {
+//     while (num > Math.Pow(10, numOfDigit))
+//     {
+//         num /= 10;
 //     }
-//     Console.Write("Третья цифра в числе: ");
-//     Console.Write(num);
-//     Console.Write(", равняется: ");
-//     Console.WriteLine(res%10);
+//     return num = num % 10;
+
 // }
-// else{
-//    Console.Write("Третья цифра в числе: ");
-//    Console.Write(num);
-//    Console.WriteLine(", отсутствует");
+// Console.WriteLine(num > Math.Pow(10, digit -1) ? "Число меньше" : GetDigit(num, digit));
+
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+double digit = Math.Log10(num);
+digit = (int)digit;
+if (digit > 1){
+    int count = 1;
+    int res = num;
+    while (count < digit -1){
+        res = res/10;
+        count++;
+    }
+    Console.Write("Третья цифра в числе: ");
+    Console.Write(num);
+    Console.Write(", равняется: ");
+    Console.WriteLine(res%10);
+}
+else{
+   Console.Write("Третья цифра в числе: ");
+   Console.Write(num);
+   Console.WriteLine(", отсутствует");
+}
+// Console.Write("Введите число: ");
+// string line = Console.ReadLine()??"0";
+// char[] digits = line.ToCharArray();
+// if (line.Length > 2){
+//    Console.WriteLine(digits[2]);
 // }
-Console.Write("Введите число: ");
-string line = Console.ReadLine()??"0";
-char[] digits = line.ToCharArray();
-if (line.Length > 2){
-   Console.WriteLine(digits[2]);
-}
-else {
-    Console.WriteLine("Третьей цифры нет");
-}
+// else {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+    // Получим элемент по ключу == day
+
 
--- Sem2Task15/Program.cs
+++ Sem2Task15/Program.cs
@@ -5,7 +5,7 @@
 
 // int day = int.Parse(Console.ReadLine() ?? "0");
 // if (day > 7 || day < 1){
-//     Console.WriteLine("Число " + day + " не является днем недели, введите число от 1 до 7! ");   
+//     Console.WriteLine("Число " + day + " не является днем недели, введите число от 1 до 7! ");  
 // }
 // else{
 //     if (day > 5){
@@ -15,17 +15,46 @@
 //     Console.WriteLine(day + " - день недели является рабочим");
 // }
 // }
-Console.WriteLine("Введите день недели");
-string day = Console.ReadLine()??"0";
+// Console.WriteLine("Введите день недели");
+// string day = Console.ReadLine()??"0";
 
-switch (day)
+// switch (day)
+// {
+//     case "1": Console.WriteLine("Этот день не выходной"); break;
+//     case "2": Console.WriteLine("Этот день не выходной"); break;
+//     case "3": Console.WriteLine("Этот день не выходной"); break;
+//     case "4": Console.WriteLine("Этот день не выходной"); break;
+//     case "5": Console.WriteLine("Этот день не выходной"); break;
+//     case "6": Console.WriteLine("Этот день выходной"); break;
+//     case "7": Console.WriteLine("Этот день выходной"); break;
+//     default: Console.WriteLine("Неверные данные"); break;
+// }
+
+// Приглашение в консоли ввести число
+Console.WriteLine("Введите положительное число");
+
+// Создаем переменную day и с консоли вводим ее значение.
+// При этом сразу проверяем на null и конвертируем  в int
+int day = int.Parse(Console.ReadLine() ?? "0");
+
+// Проверка введеного числа на день недели
+if (day > 7 || day < 1)
 {
-    case "1": Console.WriteLine("Этот день не выходной"); break;
-    case "2": Console.WriteLine("Этот день не выходной"); break;
-    case "3": Console.WriteLine("Этот день не выходной"); break;
-    case "4": Console.WriteLine("Этот день не выходной"); break;
-    case "5": Console.WriteLine("Этот день не выходной"); break;
-    case "6": Console.WriteLine("Этот день выходной"); break;
-    case "7": Console.WriteLine("Этот день выходной"); break;
-    default: Console.WriteLine("Неверные данные"); break;
+    // выводим результат в консоль
+    Console.WriteLine("Число " + day + " не является днем недели, введите число от 1 до 7! ");
+}
+else
+{
+    // Инициализация словаря со значениями
+    Dictionary<int, string> dayOfWeek = new Dictionary<int, string>
+{
+    { 1, "Понедельник - Рабочий день" },
+    { 2, "Вторник - Рабочий день" },
+    { 3, "Среда - Рабочий день" },
+    { 4, "Четверг - Рабочий день" },
+    { 5, "Пятница - Рабочий день" },
+    { 6, "Суббота - Выходной день" },
+    { 7, "Воскресенье - Выходной день" },
+};
+    Console.WriteLine(dayOfWeek[day]);
 }
