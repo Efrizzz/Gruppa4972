99052d0e1ac9ca7b11da3a2032a5f5c702f230c0
Oleg Smorodin <benmaker@mail.ru>
2023-09-20 18:32:31 +0300

    Add Sem3Task 19, 21, 23

---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,78 @@
+﻿//                                     Задача №21 
+// Напишите программу, которая принимает на вход координаты двух точек и
+// находит расстояние между ними в 3D пространстве.
+// Осуществить возможность ввода всех точек в одну строку
+
+
+// using System;
+
+// public class Answer
+// {
+//     private static double Length(int[] pointA, int[] pointB)
+//     {
+//       // Введите свое решение ниже
+// {
+//     double res = 0;
+    
+//     res = Math.Sqrt(Math.Pow(pointA[0] - pointB[0] ,2) + Math.Pow(pointA[1] - pointB[1] ,2) + Math.Pow(pointA[2] - pointB[2] ,2));
+//     return res;
+// }
+
+//     }
+
+//   // Не удаляйте и не меняйте метод Main! 
+//     public static void Main(string[] args) {
+//         int x1, x2, x3, y1, y2, y3;
+
+//         if (args.Length >= 6) {
+//             x1 = int.Parse(args[0]);
+//             x2 = int.Parse(args[1]);
+//             x3 = int.Parse(args[2]);
+//             y1 = int.Parse(args[3]);
+//             y2 = int.Parse(args[4]);
+//             y3 = int.Parse(args[5]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             x1 = 3;
+//             x2 = 6;
+//             x3 = 8;
+//             y1 = 2;
+//             y2 = 1;
+//             y3 = -7;
+//         }
+
+//         // Не удаляйте строки ниже
+//         double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+//         Console.WriteLine($"{result:F2}");
+//     }
+// }
+
+
+// Вывод сообщения и вызов метода DistanceCalculation
+double Prompt(string message1)
+{
+    Console.WriteLine(message1);
+    string values = Console.ReadLine()??",";
+
+    string[] valuesArray = values.Split(",");
+    int xA = Convert.ToInt32(valuesArray[0]);
+    int yA = Convert.ToInt32(valuesArray[1]);
+    int zA = Convert.ToInt32(valuesArray[2]);
+    int xB = Convert.ToInt32(valuesArray[3]);
+    int yB = Convert.ToInt32(valuesArray[4]);
+    int zB = Convert.ToInt32(valuesArray[5]);
+
+    return DistanceCalculation(xA, yA, zA, xB, yB, zB);
+}
+
+
+//A (3,6,8); B (2,1,-7)
+// Расчёт расстояния
+double DistanceCalculation(int xA, int yA, int zA, int xB, int yB, int zB)
+{
+    return Math.Sqrt(Math.Pow(Math.Abs(xA - xB), 2)
+                    + Math.Pow(Math.Abs(yA - yB), 2)
+                    + Math.Pow(Math.Abs(zA - zB), 2));
+}
+
+Console.WriteLine(Prompt("Введите 6 чисел через запятую (xA,yA,zA,xB,yB,zB): "));
